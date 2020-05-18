0b62466974a2abd761b890985d2f74acac676050
Diego Broetto <diego.broetto1@gmail.com>
2020-05-17 21:41:32 -0300

    Page Admin

--- views-cache/footer.1826c47489e9c7d22dce3a407b941cc9.rtpl.php
+++ views-cache/footer.1826c47489e9c7d22dce3a407b941cc9.rtpl.php
@@ -82,8 +82,8 @@
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 <!-- jQuery sticky menu -->
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.sticky.js"></script>
+<script src="/res/site/js/owl.carousel.min.js"></script>
+<script src="/res/site/js/jquery.sticky.js"></script>
 
 <!-- jQuery easing -->
 <script src="/res/site/js/jquery.easing.1.3.min.js"></script>
