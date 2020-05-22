92f874943040a2146d5e2c48ed8613294836be71
Diego Broetto <diego.broetto1@gmail.com>
2020-05-22 03:30:13 -0300

    criado tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -29,7 +31,9 @@ class Page{
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if($this->options["header"] === true){
+            $this->tpl->draw("header");
+        }
 
  
     }
@@ -50,13 +54,14 @@ class Page{
 
         return $this->tpl->draw($name, $returnHTML);
 
-    
     }
 
     public function __destruct(){
         
-        $this->tpl->draw("footer");
-
+        if($this->options["footer"] === true){
+            $this->tpl->draw("footer");
+        }
+    
     }
 
 
